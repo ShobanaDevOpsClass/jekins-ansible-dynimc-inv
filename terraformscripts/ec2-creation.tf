812e19a373900ba416152c9aeab356f591fb3763
shobananaveen <92475524+shobananaveen@users.noreply.github.com>
2025-02-23 14:39:51 -0600

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,13 +1,12 @@
 provider "aws"{
-region = "ap-south-1"
+region = "us-east-1"
 }
-
 resource "aws_instance" "AWSEC2Instance"{
-   ami = "ami-0d682f26195e9ec0f"
-   instance_type = "t2.micro"
-   key_name = "devopsjan2025"
-   security_groups = ["launch-wizard-1"]
-   tags={
-   Name = "terraformserver"
-   }
+ami = "ami-05b10e08d247fb927"
+instance_type = "t2.micro"
+key_name = "devopsdec2024"
+security_groups = ["launch-wizard-1"]
+tags={
+Name = "terraformserver"
+}
 }
